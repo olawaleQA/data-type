1e99006028b1c973e909526287d5070a44e51db6
olawaleQA <olawaleolowo19@gmail.com>
2024-11-22 11:16:51 +0100

    first commit

---
+++ days.js
@@ -0,0 +1,10 @@
+// const myAge = 45
+
+// const multiplyAge = myAge * 2
+// console.log(multiplyAge)
+
+const Day = "Wednesday" 
+const position = "3rd"
+console.log(position)
+const output =  "Today is " + Day + '. ' + 'it is the ' + position + ' day of the week'
+console.log(output)
